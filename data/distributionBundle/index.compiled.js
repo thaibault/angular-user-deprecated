1624a9dcb72ea69d3d5c4cecdf198a3a302ad6d4
torben <t.sickert[~at~]gmail.com>
2016-09-15 16:54:58 +0200

    Update project homepage content.

---
+++ data/distributionBundle/index.compiled.js
@@ -0,0 +1,2 @@
+'use strict';
+!function(e,t){"object"==typeof exports&&"object"==typeof module?module.exports=t():"function"==typeof define&&define.amd?define("angular-user",[],t):"object"==typeof exports?exports["angular-user"]=t():e["angular-user"]=t()}(this,function(){return function(e){function t(r){if(o[r])return o[r].exports;var n=o[r]={exports:{},id:r,loaded:!1};return e[r].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var o={};return t.m=e,t.c=o,t.p="",t(0)}([function(e,t,o){e.exports=o(1)},function(e,t){"use strict"}])});
