980642185979583900f28c3462b3d91c9dc5b5bb
torben <t.sickert[~at~]gmail.com>
2016-11-23 11:44:24 +0100

    Automatic update to new version.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+// @flow
+// #!/usr/bin/env node
+// -*- coding: utf-8 -*-
+/** @module angularUser */
+'use strict'
+/* !
+    region header
+    [Project page](http://torben.website/angularUser)
 
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons
+    naming 3.0 unported license.
+    See http://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
+// region imports
+// endregion
+// region plugins/classes
+// endregion
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
