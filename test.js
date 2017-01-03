a45ca24bfc88f34b37f93761848f11f4b2c22bf7
torben <t.sickert[~at~]gmail.com>
2017-01-03 18:23:47 +0100

    Automatic update to new version.

--- test.js
+++ test.js
@@ -22,7 +22,9 @@ import PouchDBAdabterMemory from 'pouchdb-adapter-memory'
 declare var DEBUG:boolean
 declare var TARGET_TECHNOLOGY:string
 // endregion
-registerTest(function(roundType:string, targetTechnology:?string, $:any):void {
+registerTest(async function(
+    roundType:string, targetTechnology:?string, $:any
+):Promise<void> {
     // region mocking angular environment
     $('head').append('<base href="/">')
     const self:Object = this
@@ -47,14 +49,18 @@ registerTest(function(roundType:string, targetTechnology:?string, $:any):void {
         process.setMaxListeners(30)
     }
     const hammerjs:Object = require('hammerjs')
-    const core:Object = require('@angular/core')
-    const {Component, enableProdMode, NgModule} = core
+    const {DebugElement, Component, enableProdMode, NgModule} = require(
+        '@angular/core')
+    const {ComponentFixture, TestBed} = require('@angular/core/testing')
+    const By:Object = require('@angular/platform-browser').By
     const platformBrowserDynamic:Function = require(
         '@angular/platform-browser-dynamic'
     ).platformBrowserDynamic
+    const {BrowserDynamicTestingModule, platformBrowserDynamicTesting} =
+        require('@angular/platform-browser-dynamic/testing')
     const RouterModule:Object = require('@angular/router').RouterModule
     const index:Object = require('./index')
-    const UserModule = index.default
+    const UserModule:Object = index.default
     const {
         AuthenticationGuard,
         LoginComponent
@@ -75,22 +81,59 @@ registerTest(function(roundType:string, targetTechnology:?string, $:any):void {
         ]
     })
     // endregion
-    class ApplicationModule {
+    // region test services
+    class Module {
         constructor(authentication:AuthenticationGuard):void {
-            // region tests
-            self.test('AuthenticationGuard', (assert:Object):void => {
+            self.test(`AuthenticationGuard (${roundType})`, (
+                assert:Object
+            ):void => {
                 assert.strictEqual('TODO', 'TODO')
             })
-            self.test('LoginComponent', (assert:Object):void => {
-                assert.strictEqual('TODO', 'TODO')
-            })
-            // endregion
         }
     }
     if (!DEBUG)
         enableProdMode()
-    this.test('UserModule', (assert:Object):void => assert.ok(
-        platformBrowserDynamic().bootstrapModule(ApplicationModule)))
+    this.module(`UserModule.services (${roundType})`)
+    let platform:Object
+    let module:Object
+    try {
+        platform = platformBrowserDynamic()
+        module = await platform.bootstrapModule(Module)
+    } catch (error) {
+        throw error
+    }
+    // NOTE: Simply doing "await new Promise(..." doesn't work yet.
+    const serviceTests:Promise<void> = new Promise((resolve:Function):void => {
+        let done:boolean = false
+        this.moduleDone(():void => {
+            if (done)
+                return
+            done = true
+            module.destroy()
+            platform.destroy()
+            resolve()
+        })
+    })
+    // endregion
+    // region test components
+    serviceTests.then(():void => {
+        this.module(`UserModule.components (${roundType})`)
+        TestBed.initTestEnvironment(
+            BrowserDynamicTestingModule, platformBrowserDynamicTesting()
+        ).configureTestingModule({imports: [Module]})
+        this.test(`LoginComponent (${roundType})`, async (
+            assert:Object
+        ):void => {
+            const done:Function = assert.async()
+            await TestBed.compileComponents(LoginComponent)
+            const fixture = TestBed.createComponent(LoginComponent)
+            fixture.componentInstance.model = {disabled: true}
+            fixture.componentInstance.ngOnInit()
+            assert.strictEqual(fixture.componentInstance.errorMessage, '')
+            done()
+        })
+    })
+    // endregion
 }, ['full'])
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
