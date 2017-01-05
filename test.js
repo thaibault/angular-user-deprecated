2757d267d9d6473a1b2427844f9854188ed7c5d8
torben <t.sickert[~at~]gmail.com>
2017-01-05 17:03:18 +0100

    Automatic update to new version.

--- test.js
+++ test.js
@@ -15,7 +15,7 @@
     endregion
 */
 // region imports
-import registerTest from 'clientnode/test'
+import registerAngularTest from 'angular-generic/test'
 import PouchDBAdabterMemory from 'pouchdb-adapter-memory'
 // NOTE: Only needed for debugging this file.
 try {
@@ -26,57 +26,30 @@ try {
 declare var DEBUG:boolean
 declare var TARGET_TECHNOLOGY:string
 // endregion
-registerTest(async function(
-    roundType:string, targetTechnology:?string, $:any
-):Promise<void> {
-    // region mocking angular environment
-    $('head').append('<base href="/">')
-    const self:Object = this
-    $.global.genericInitialData = {configuration: {
-        database: {
-            url: 'test',
-            options: {adapter: 'memory'},
-            plugins: [PouchDBAdabterMemory]
-        },
-        test: true
-    }}
-    /*
-        NOTE: A working polymorphic angular environment needs some assumptions
-        about the global scope, so mocking and initializing that environment
-        after a working browser environment is present.
-    */
-    if (TARGET_TECHNOLOGY === 'node') {
-        global.window = $.global
-        global.document = $.context
-        global.Element = $.global.Element
-        global.window.Reflect = global.Reflect
-        process.setMaxListeners(30)
-    }
-    require('hammerjs')
-    const {Component, enableProdMode, NgModule} = require('@angular/core')
-    const {TestBed} = require('@angular/core/testing')
-    const {By} = require('@angular/platform-browser')
-    const {platformBrowserDynamic} = require(
-        '@angular/platform-browser-dynamic')
-    const {BrowserDynamicTestingModule, platformBrowserDynamicTesting} =
-        require('@angular/platform-browser-dynamic/testing')
-    const {Router, RouterModule} = require('@angular/router')
-    const {RouterTestingModule} = require('@angular/router/testing')
-    const index:Object = require('./index')
-    const UserModule:Object = index.default
+registerAngularTest({bootstrap: function(
+    ApplicationComponent:Object, roundType:string,
+    targetTechnology:?string, $:any
+):Array<Object> {
+    // region prepare services
+    $.global.genericInitialData = {configuration: {database: {
+        url: 'test',
+        options: {adapter: 'memory'},
+        plugins: [PouchDBAdabterMemory]
+    }}}
     const {
         RouterLinkStubDirective, RouterOutletStubComponent, RouterStub,
         ActivatedRouteStub
     } = require('angular-generic/mockup')
+    const {NgModule} = require('@angular/core')
+    const {Router, RouterModule} = require('@angular/router')
+    const index:Object = require('./index')
+    const UserModule:Object = index.default
     const {
         AuthenticationGuard,
         LoginComponent
     } = index
-    @Component({
-        selector: '#qunit-fixture',
-        template: '<div>Application<router-outlet></router-outlet></div>'
-    })
-    class ApplicationComponent {}
+    const self:Object = this
+    // IgnoreTypeCheck
     @NgModule({
         bootstrap: [ApplicationComponent],
         declarations: [ApplicationComponent],
@@ -98,35 +71,8 @@ registerTest(async function(
             })
         }
     }
-    if (!DEBUG)
-        enableProdMode()
     this.module(`UserModule.services (${roundType})`)
-    let platform:Object
-    let module:Object
-    try {
-        platform = platformBrowserDynamic()
-        module = await platform.bootstrapModule(Module)
-    } catch (error) {
-        throw error
-    }
-    this.load()
-    await new Promise((resolve:Function):void => {
-        let done:boolean = false
-        this.moduleDone(():void => {
-            if (done)
-                return
-            done = true
-            module.destroy()
-            platform.destroy()
-            resolve()
-        })
-    })
-    // endregion
-    // region test components
-    this.module(`UserModule.components (${roundType})`)
-    TestBed.initTestEnvironment(
-        BrowserDynamicTestingModule, platformBrowserDynamicTesting()
-    ).configureTestingModule({
+    return [Module, {
         declarations: [
             ApplicationComponent,
             RouterLinkStubDirective,
@@ -134,18 +80,22 @@ registerTest(async function(
         ],
         imports: [UserModule],
         providers: [{provide: Router, useClass: RouterStub}]
-    })
-    await TestBed.compileComponents()
-    this.test(`LoginComponent (${roundType})`, async (
-        assert:Object
-    ):Promise<void> => {
+    }]
+    // endregion
+}, component: function(TestBed:Object, roundType:string):void {
+    // region prepare components
+    const {LoginComponent} = require('./index')
+    // endregion
+    // region test components
+    this.module(`UserModule.components (${roundType})`)
+    this.test(`LoginComponent (${roundType})`, (assert:Object):void => {
         const {componentInstance} = TestBed.createComponent(
             LoginComponent)
         componentInstance.model = {disabled: true}
         assert.ok(componentInstance._authentication)
     })
     // endregion
-}, ['full'])
+}, '<router-outlet></router-outlet>')
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
 // vim: foldmethod=marker foldmarker=region,endregion:
